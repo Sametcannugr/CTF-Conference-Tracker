5799c5406455f0985da28b153eeecadbc1f1a535
Sametcannugr <sametcancan123123@gmail.com>
2026-01-20 17:07:25 +0300

    Ciktilari dosyaya kaydetme özelligi eklendi

---
+++ src/baslat.sh
@@ -0,0 +1,7 @@
+echo "CTF Taramasi Baslatiliyor..."
+
+# python main.py > ctf_sonuclar.txt komutu, 
+# ekrana basilan yazilari yakalayip dosyaya kaydeder (Unix I/O Stream).
+python main.py > ctf_sonuclar.txt
+
+echo "Islem bitti! Sonuclar 'ctf_sonuclar.txt' dosyasina kaydedildi."
